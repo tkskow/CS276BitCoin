fe9e5d6f841fbb86573e1241adb2de1b15ce9b77
Eirik Klevstad <Eirik.Klevstad@gmail.com>
2014-11-12 18:23:25 -0800

    Oppdatert med databasekode. Støtter update.

--- Locator.py
+++ Locator.py
@@ -1,15 +1,19 @@
 from blockchain import blockexplorer, exceptions
 from collections import OrderedDict
-import requests, json, pymongo
+import requests, json, pymongo, datetime, time
  
-location_service_url = "http://www.geoplugin.net/json.gp?ip="
-sort_order = ["geoplugin_request","geoplugin_city", "geoplugin_longitude", "geoplugin_latitude", "geoplugin_region", "geoplugin_regionName", "geoplugin_countryCode", "geoplugin_countryName", "geoplugin_continentCode", "geoplugin_regionCode", "geoplugin_areaCode", "geoplugin_dmaCode"]
+LATEST_HASH_URL = "https://blockchain.info/q/latesthash"
+LOCATION_SERVICE_URL = "http://www.geoplugin.net/json.gp?ip="
 
+SORT_ORDER = ["geoplugin_request","geoplugin_city", "geoplugin_longitude", "geoplugin_latitude", "geoplugin_region", "geoplugin_regionName", "geoplugin_countryCode", "geoplugin_countryName", "geoplugin_continentCode", "geoplugin_regionCode", "geoplugin_areaCode", "geoplugin_dmaCode"]
 
-dickRelay = {}
-dickInitial ={}
+RELAYED_BY_IPS = {}
+INITIAL_IPS = {}
 
-def check_block(block):
+LATEST_BLOCK_HASH = ""
+#INITIAL_IPS = {"107.170.219.42" : 17, "146.185.142.86" : 6, "192.241.230.87" : 28, "182.241.230.87" : 8, "147.145.32.14" : 1}
+
+def traverse_block(block):
 
 	skip = True
 	for transaction in block.transactions:
@@ -19,64 +23,109 @@ def check_block(block):
 		else:
 			relayed_by = transaction.relayed_by
 
-			if not relayed_by in dickRelay:
-				dickRelay[relayed_by] = 1
+			if not relayed_by in RELAYED_BY_IPS:
+				RELAYED_BY_IPS[relayed_by] = 1
 			else:
-				dickRelay[relayed_by] += 1
+				RELAYED_BY_IPS[relayed_by] += 1
 
 			try:
 				inv = blockexplorer.get_inventory_data(transaction.hash)
 				initial_ip = inv.initial_ip
 
-				if not initial_ip in dickInitial:
-					dickInitial[initial_ip] = 1
+				if not initial_ip in INITIAL_IPS:
+					INITIAL_IPS[initial_ip] = 1
 				else:
-					dickInitial[initial_ip] += 1
+					INITIAL_IPS[initial_ip] += 1
 
 			except Exception, e:
 				print e
 
-def add_to_database():
-	client = pymongo.MongoClient('localhost', 27017)
+def create_document(ip_address, ip_count):
 
-	connection = client['mynewdb']
+	# query geolocation about location of ip address
+	response = requests.get(LOCATION_SERVICE_URL + ip_address)
+	document = response.json()
 
-	db = connection.delete.testdata
-
-	for ip in sorted(dickInitial, key=dickInitial.get, reverse=True):
-
-		response = requests.get(location_service_url + ip)
-		data = response.json()
-		db.insert(format_geotag(data))
-
-	client.close()
-
-def format_geotag(existing):
-
-	existing.pop("geoplugin_currencyConverter")
-	existing.pop("geoplugin_currencySymbol")
-	existing.pop("geoplugin_currencySymbol_UTF8")
-	existing.pop("geoplugin_credit")
-	existing.pop("geoplugin_currencyCode")
-	existing.pop("geoplugin_status")
+	# remove stupid stuff
+	document.pop("geoplugin_currencyConverter")
+	document.pop("geoplugin_currencySymbol")
+	document.pop("geoplugin_currencySymbol_UTF8")
+	document.pop("geoplugin_credit")
+	document.pop("geoplugin_currencyCode")
+	document.pop("geoplugin_status")
 
 	orderedJson = OrderedDict()
-
-	for item in sorted(existing.iteritems(), key=lambda (k, v): sort_order.index(k)):
+	for item in sorted(document.iteritems(), key=lambda (k, v): SORT_ORDER.index(k)):
 		orderedJson[item[0]] = item[1]
+	
+	# add count and timestamp
+	orderedJson["count"] = str(ip_count)
+	orderedJson["last_updated"] = str(datetime.datetime.utcnow())
 	
 	return orderedJson
 
+def update_database():
+	client = pymongo.MongoClient('localhost', 27017)
+	connection = client['mynewdb']
+	db_collection = connection.testdocuments
+
+	for ip in sorted(INITIAL_IPS, key=INITIAL_IPS.get, reverse=True):
+		ip_count = INITIAL_IPS[ip]
+
+		document = create_document(ip, ip_count)
+
+		existing_document = db_collection.find( { "geoplugin_request" : str(ip) } ).limit(1)	
+
+		if(existing_document.count() > 0):
+			# ip already in database. Let's update it.
+			current_count = existing_document[0]['count']
+			db_collection.update(
+				{ "geoplugin_request" : str(ip)},
+				{ "$set":
+					{
+						'count' : str(int(current_count) + int(ip_count)),
+						'last_updated' : document['last_updated']
+					}
+				})
+		else:
+			# add ip to database with count
+			db_collection.insert(document_formatted)
+
+	client.close()
+
+def wait_for_new_block():
+
+	response = requests.get(LATEST_HASH_URL)
+	latest_hash = response.text
+
+	while(latest_hash == LATEST_BLOCK_HASH):
+		# Sleep for how long?
+		time.sleep(480) # 8 minutes?
+		response = requests.get(LATEST_HASH_URL)
+		latest_hash = response.text
+
+	LATEST_BLOCK_HASH = latest_hash
+
+def clean_up():
+	INITIAL_IPS = {}
+	RELAYED_BY_IPS = {}
+
+def main():
+
+	testrun = 0
+	while(testrun > 10):
+		wait_for_new_block()
+
+		latest_block = blockexplorer.get_block(LATEST_BLOCK_HASH)
+
+		traverse_block(latest_block)
+
+		update_database()
+
+		clean_up()
+
+		testrun += 1
 
 
-latest_block = blockexplorer.get_latest_block()
-
-block = blockexplorer.get_block(str(latest_block.block_index))
-
-check_block(block)
-
-add_to_database()
-
-
-
+main()
 
